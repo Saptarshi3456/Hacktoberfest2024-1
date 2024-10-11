b64ab92c2776d8e1a971a40c027c371ab6b89086
Saptarshi3456 <basaksaptarshi1@gmail.com>
2024-10-11 09:26:17 +0530

    Create AreaCircumference.py

---
+++ AreaCircumference.py
@@ -0,0 +1,12 @@
+import math
+
+# Taking radius input from the user
+radius = float(input(&quot;Enter the radius of the circle: &quot;))
+
+# Calculating area and circumference
+area = math.pi * radius**2
+circumference = 2 * math.pi * radius
+
+# Displaying the results
+print(f&quot;Area of the circle: {area}&quot;)
+print(f&quot;Circumference of the circle: {circumference}&quot;)
